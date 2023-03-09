875532afd44888e0469af7354724326caf4b8831
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2023-03-08 21:36:17 -0800

    Solve divSquares unique ID issue

--- main.js
+++ main.js
@@ -3,18 +3,19 @@ function defaultGrid(col, row) {
         let rootDiv = document.createElement("div");
         
         rootDiv.classList.add("rootDiv", "rootDiv"+c);
-        rootDiv.setAttribute("id","rootDiv");
+        rootDiv.setAttribute("id","rootDiv"+c);
         document.querySelector(".container").append(rootDiv);
         
         let poop = c;
+        let interval = 16 * c;
         for (let r = 0; r < row; r++) {
             newRow = document.createElement("div");
             newRow.classList.add("divSquares"); 
-            newRow.setAttribute("id","divSquares");
+            newRow.setAttribute("id",`divSquares${r+interval}`);
             document.querySelector(".rootDiv"+poop).append(newRow);
         }
     }
-}
+};
 
 function resetPage() {
     defaultGrid() // pass function into default grid? that gets user input?
@@ -22,10 +23,9 @@ function resetPage() {
 
 defaultGrid(16, 16);
 
-let divSquares = document.querySelectorAll(".divSquares");
-divSquares.addEventListener("mouseover", changeColor);
-
-function changeColor() {
-    let changeTo = document.querySelectorAll(".divSquares");
-    changeTo.setAttribute("style", "color:blue");
-}
+for (i = 0; i < (16*16); i++) {
+    let divSquares = document.getElementById("divSquares"+i);
+    divSquares.addEventListener("mouseover", (e) => {
+        divSquares.style.backgroundColor = "blue";
+    })
+};
