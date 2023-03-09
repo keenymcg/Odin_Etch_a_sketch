8dcef50261b21d74ccecceb75ffb3a32634fc5ff
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2023-03-08 17:13:09 -0800

    Add CSS hover, but color doesn't remain

--- main.js
+++ main.js
@@ -2,18 +2,30 @@ function defaultGrid(col, row) {
     for (let c = 0; c < col; c++) {
         let rootDiv = document.createElement("div");
         
-        rootDiv.classList.add("rootDiv", "rootDiv"+c); //also add "hover" class?
+        rootDiv.classList.add("rootDiv", "rootDiv"+c);
+        rootDiv.setAttribute("id","rootDiv");
         document.querySelector(".container").append(rootDiv);
         
         let poop = c;
         for (let r = 0; r < row; r++) {
             newRow = document.createElement("div");
-            newRow.classList.add("rowFloat"); 
+            newRow.classList.add("divSquares"); 
+            newRow.setAttribute("id","divSquares");
             document.querySelector(".rootDiv"+poop).append(newRow);
         }
-        // todo: stack the rows -- maybe add breaks, or use flex
     }
+}
+
+function resetPage() {
+    defaultGrid() // pass function into default grid? that gets user input?
 }
 
 defaultGrid(16, 16);
 
+let divSquares = document.querySelectorAll(".divSquares");
+divSquares.addEventListener("mouseover", changeColor);
+
+function changeColor() {
+    let changeTo = document.querySelectorAll(".divSquares");
+    changeTo.setAttribute("style", "color:blue");
+}
