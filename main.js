cfaf4560e9857bf4593d9053b275d7543c9ea92c
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2023-03-09 20:12:24 -0800

    Dynamic grid, but changes cause break

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-function defaultGrid(col, row) {
-    for (let c = 0; c < col; c++) {
+function resetGrid() {
+    
+}
+
+function defaultGrid(newVal) {
+    // may need reset at the start of this default grid
+    for (let c = 0; c < newVal; c++) {
         let rootDiv = document.createElement("div");
         
         rootDiv.classList.add("rootDiv", "rootDiv"+c);
@@ -7,28 +12,16 @@ function defaultGrid(col, row) {
         document.querySelector(".container").append(rootDiv);
         
         let poop = c;
-        let interval = col * c;
-        for (let r = 0; r < row; r++) {
+        let interval = newVal * c;
+        for (let r = 0; r < newVal; r++) {
             newRow = document.createElement("div");
             newRow.classList.add("divSquares"); 
             newRow.setAttribute("id",`divSquares${r+interval}`);
-            newRow.setAttribute("style", `width: ${500/row}px; height: ${500/row}px;`)
+            newRow.setAttribute("style", `width: ${500/newVal}px; height: ${500/newVal}px;`)
             document.querySelector(".rootDiv"+poop).append(newRow);
-        }
+        } 
+    resetGrid();
     }
-};
-
-function resetPage() {
-    defaultGrid() // pass function into default grid? that gets user input?
-}
-
-defaultGrid(16, 16);
-
-for (i = 0; i < (16*16); i++) {
-    let divSquares = document.getElementById("divSquares"+i);
-    divSquares.addEventListener("mouseover", (e) => {
-        divSquares.style.backgroundColor = "blue";
-    })
 };
 
 const slider = document.querySelector('#slider');
@@ -36,9 +29,19 @@ const screenVal = document.querySelector('.value');
 slider.addEventListener('input', function() {
     let val = document.getElementById("slider").value;
     screenVal.textContent = val;
+    let val2 = parseInt(val);
+    defaultGrid(val2);
+    // console.log(val2);
+    // console.log(typeof val); //returns string
+    for (i = 0; i < (val2*val2); i++) {
+        let divSquares = document.getElementById("divSquares"+i);
+        divSquares.addEventListener("mouseover", (e) => {
+            divSquares.style.backgroundColor = "blue";
+        })
+    };
 });
 
-// THIS GETS OUR GRID VALUE
+// THIS was supposed to GETS OUR GRID VALUE
 let newVal = 0;
 function sliderValue() {
     slider.addEventListener('input', function() {
@@ -52,7 +55,3 @@ function sliderValue() {
 };
 
 sliderValue();
-
-// want the slider to affect the row value
-// col in defaultGrid is redundant, remove and replace cols with rows
-// maybe I pass into defaultGrid( anon func()) a function the returns the slider value
